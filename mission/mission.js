0cd1c98238a02a0ac4395ca120f33649dff25284
j-rubio <esc18010@byui.edu>
2025-04-30 15:03:03 -0500

    adding dark mode with a select element

---
+++ mission/mission.js
@@ -0,0 +1,14 @@
+const themeSelector = document.querySelector('select')
+const footerLogo = document.getElementById('byui-logo_footer')
+
+function changeTheme() {
+  if (themeSelector.value == 'dark') {
+    document.body.setAttribute('class', 'dark')
+    footerLogo.setAttribute('src', 'byui-logo_white.png')
+  } else {
+    document.body.removeAttribute('class')
+    footerLogo.setAttribute('src', 'byui-logo_blue.webp')
+  }
+}
+
+themeSelector.addEventListener('change', changeTheme)
