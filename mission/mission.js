50c973d47d53dd3d90497ffe1251425c2769e028
j-rubio <esc18010@byui.edu>
2025-04-30 15:10:24 -0500

    change path format to logo images

--- mission/mission.js
+++ mission/mission.js
@@ -4,10 +4,10 @@ const footerLogo = document.getElementById('byui-logo_footer')
 function changeTheme() {
   if (themeSelector.value == 'dark') {
     document.body.setAttribute('class', 'dark')
-    footerLogo.setAttribute('src', 'byui-logo_white.png')
+    footerLogo.setAttribute('src', './byui-logo_white.png')
   } else {
     document.body.removeAttribute('class')
-    footerLogo.setAttribute('src', 'byui-logo_blue.webp')
+    footerLogo.setAttribute('src', './byui-logo_blue.webp')
   }
 }
 
