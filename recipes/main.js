3bce9ee126b7b22a6e071c373212107e881d61c8
j-rubio <esc18010@byui.edu>
2025-06-27 12:15:30 -0500

    recipes.css cleaned of comments

--- recipes/main.js
+++ recipes/main.js
@@ -29,7 +29,6 @@ function recipeTemplate(recipe) {
 }
 
 function tagsTemplate(tags) {
-  // loop through the tags list and transform the strings to HTML
   let html = ''
   tags.forEach((element) => {
     html += `<div>${element}</div>`
@@ -38,44 +37,29 @@ function tagsTemplate(tags) {
 }
 
 function ratingTemplate(rating) {
-  // begin building an html string using the ratings HTML written earlier as a model.
   let html = `<span
 	class="rating"
 	role="img"
-	aria-label="Rating: ${rating} out of 5 stars"
->`
-  // our ratings are always out of 5, so create a for loop from 1 to 5
+	aria-label="Rating: ${rating} out of 5 stars">`
   for (let i = 1; i < 6; i++) {
-    // check to see if the current index of the loop is less than our rating
-    // if so then output a filled star
     if (i <= rating) {
       html += `<span aria-hidden="true" class="icon-star">⭐</span>`
-    } // else output an empty star
-    else {
+    } else {
       html += `<span aria-hidden="true" class="icon-star-empty">☆</span>`
     }
   }
-  // after the loop, add the closing tag to our string
   html += `</span>`
-  // return the html string
   return html
 }
 
 function renderRecipes(recipeList) {
-  // get the element we will output the recipes into
   const recipesHolder = document.querySelector('.recipes')
-
-  // use the recipeTemplate function to transform our recipe objects into recipe HTML strings
   const html = recipeList.map(recipeTemplate).join('')
-
-  // Set the HTML strings as the innerHTML of our output element.
   recipesHolder.innerHTML = html
 }
 
 function init() {
-  // get a random recipe
   const recipe = getRandomListEntry(recipes)
-  // render the recipe with renderRecipes.
   renderRecipes([recipe])
 }
 init()
@@ -104,6 +88,5 @@ function searchHandler(event) {
   renderRecipes(sortedRecipes)
 }
 
-// buttonSearch.addEventListener('click', searchHandler)
 const form = document.querySelector('form')
 form.addEventListener('submit', searchHandler)
